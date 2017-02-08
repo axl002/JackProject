4136ccc94323dd7615051e2b8e30d610e675939e
axl002.ta <axl002.ta@gmail.com>
2017-02-07 20:57:47 -0800

    made scrper more efficient

--- src/main/java/JackTest.java
+++ src/main/java/JackTest.java
@@ -36,19 +36,19 @@ public class JackTest {
 
         //kafka config
 
-        Properties props = new Properties();
+//        Properties props = new Properties();
         // THIS WORKS NOW
-        props.put("bootstrap.servers", "localhost:9092");
+//        props.put("bootstrap.servers", "localhost:9092");
         //props.put("metadata.broker.list", "broker1:9092,broker2:9092");
-        props.put("acks", "all");
-        props.put("retries", 0);
-        props.put("batch.size", 20000000);
-        props.put("linger.ms", 1);
-        props.put("buffer.memory", 33554432);
+//        props.put("acks", "all");
+//        props.put("retries", 0);
+//        props.put("batch.size", 20000000);
+//        props.put("linger.ms", 1);
+//        props.put("buffer.memory", 33554432);
 
         // change this to a time stamp serializer
-        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+//        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+//        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
        // producer = new KafkaProducer<String, String>(props);
 
